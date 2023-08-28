c9f5f0df18461f5051d17faaa7f93dce7f5581fa
DouglasGuacaran <douglasguacaran@gmail.com>
2023-08-28 11:07:27 -0400

    Agrego grafico Dolar y Euro con Echarts

--- src/components/templates/Inicio.js
+++ src/components/templates/Inicio.js
@@ -5,10 +5,12 @@ import Image from '../../../src/Logo1.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import * as echarts from 'echarts';;
 
 
 function Home() {
   const [loading, setLoading] = useState(true);
+  const [historicalData, setHistoricalData] = useState({});
   const valorDolar = useSelector((state) => state.valorDolar);
   const valorEuro = useSelector((state) => state.valorEuro);
   const valorBitcoin = useSelector((state) => state.valorBitcoin);
@@ -18,6 +20,82 @@ function Home() {
     }, 400);
     return () => clearTimeout(timer);
   }, []);
+
+  useEffect(() => {
+    const fetchHistoricalData = async () => {
+      try {
+        const currencies = ['dolar', 'euro'];
+        const data = {};
+
+        for (const currency of currencies) {
+          const response = await fetch(`https://mindicador.cl/api/${currency}`);
+          const currencyData = await response.json();
+          data[currency] = currencyData.serie;
+        }
+
+        setHistoricalData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching historical data:', error);
+      }
+    };
+
+    fetchHistoricalData();
+  }, []);
+  useEffect(() => {
+    if (!loading && Object.keys(historicalData).length > 0) {
+      const chartElement = echarts.init(document.getElementById('graph'));
+      const dolarData = historicalData.dolar;
+      console.log(dolarData)
+      const euroData = historicalData.euro;
+      const dates = dolarData.map((e) => {
+        const date = new Date(e.fecha);
+        const day = date.getDate();
+        const month = date.getMonth() + 1; // Los meses en JavaScript están basados en 0, por lo que debes sumar 1
+        const year = date.getFullYear();
+        const formattedDate = `${day}/${month}/${year}`;
+        return formattedDate;
+      }).reverse()
+      const dolarValues = dolarData.map((e) => e.valor).reverse();
+      const euroValues = euroData.map((e) => e.valor).reverse();
+
+      const option = {
+        title: {
+          text: 'Dólar y Euro',
+        },
+        tooltip: {
+          trigger: 'axis',
+        },
+        legend: {
+          data: ['Dólar', 'Euro'],
+        },
+        xAxis: {
+          type: 'category',
+          data: dates,
+          nameLocation: 'middle'
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            name: 'Dólar',
+            type: 'line',
+            data: dolarValues,
+          },
+          {
+            name: 'Euro',
+            type: 'line',
+            data: euroValues
+          },
+          
+        ],
+      };
+
+      chartElement.setOption(option);
+    }
+  }, [loading, historicalData]);
+
 
   return (
     <div className="body">
@@ -48,6 +126,12 @@ function Home() {
           <Col className='col-money' xs lg="3">
             <span style={{"fontWeight": "bold"}}>Valor Bitcoin</span>
             <span>{loading ? ('Cargando...'): valorBitcoin}</span>
+          </Col>
+        </Row>
+        <Row className="row">
+          <Col className="col">
+            <div id="graph" style={{ borderRadius: '5px' ,backgroundColor:'white', width: '100%', height: '400px' }}></div>
+          
           </Col>
         </Row>
       </Container>
